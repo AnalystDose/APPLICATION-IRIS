295444d412301731ffc2635ccbf5b065001a492f
cheick007 <datapower76@gmail.com>
2024-05-13 23:15:43 +0000

    dossier gestion de vente supprimer

--- Gestion des ventes avec IA(Django)/homepage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('about/', views.AboutView.as_view(), name='about'),
-    path('prediction/', views.PredictionView.as_view(), name='prediction')
-]
